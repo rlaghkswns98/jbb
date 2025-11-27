e3e357717739617782a757ee89686ef2cfb3a584
rlaghkswns98 <rlaghkswns98@gmail.com>
2025-11-27 12:19:48 +0900

    배열의 한계 (수량을 직접 관리)

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        //       System.out.println(new Article().id);
-        //     System.out.println(new Article().id);
-        //      System.out.println(new Article().id);
-        //     System.out.println(new Article().id);
-        //      System.out.println(new Article().id);
-        //      System.out.println(new Article().id);
-        //     System.out.println(new Article().id);
-        //     System.out.println(new Article().id);
-        //     System.out.println(new Article().id);
-        //     System.out.println(new Article().id);
-        //    System.out.println(new Article().id);
-        //      System.out.println(new Article().id);
-        // }
-        Article[] articles = new Article[10];        //배열 선언 --> 생성 --> 크기가 10 으로 선언 --> 모두 null 값
-        articles[0] = new Article();
-        articles[1] = new Article();
-        articles[3] = new Article();
-        articles[4] = new Article();
-        articles[5] = new Article();
-        articles[6] = new Article();
-
-        for (Article article : articles) {     //인덱스 7,8,9 객체가 없음 --> null 값이라 NPE 발생
-            System.out.println(article.id);    //향상된 for문은 배열의 크기 만큼 반복
-        }
+        exam01();
     }
+
+
+
+
+        static void exam01() {
+
+            int articleLength = 0 ;
+            Article[] articles = new Article[10];
+
+            articles[0] = new Article();
+            articleLength++;
+            articles[1] = new Article();
+            articleLength++;
+            articles[2] = new Article();
+            articleLength++;
+            articles[3] = new Article();
+            articleLength++;
+            articles[4] = new Article();
+            articleLength++;
+            articles[5] = new Article();
+            articleLength++;
+
+            for (int i = 0 ; i < articleLength ; i++) {
+                Article article = articles[i];
+                System.out.println(article.id);
+            }
+        }
 }
 class Article {
     static int lastId;
